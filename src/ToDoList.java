dd81e835f37be24927a1f2dfbd39c4f3e1ec86b0
csbsantos <claudiasbsantos13@gmail.com>
2025-10-12 16:23:35 +0100

    Update ToDoList.java

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class ToDoList {
-}
+
+    Queue <ToDoItem> toDoList1= new PriorityQueue<ToDoItem>();
+    ToDoItem toDoItem= new ToDoItem("do homework", "do all the exercises", false, 2 );
+    Iterator <ToDoItem> iterator1= toDoList1.iterator();
+
+    public void addTask (ToDoItem toDoItem){
+        if (!toDoList1.contains(toDoItem)) {
+            toDoList1.add(toDoItem);
+        }else{
+            throw new RuntimeException("This task already exists in your list. Please, add a different one");
+
+        }
+    }
+    public String removeTask (ToDoItem toDoItem){
+        if (!toDoList1.contains(toDoItem)){
+            System.out.println("Sorry, but you can't remove something that doesn't exist. Please, review your task and try again");
+        }else{
+            toDoList1.remove(toDoItem);
+        }
+        return "";
+    }
+    public ToDoItem getNextTask (){
+        if (iterator1.hasNext()){
+            return iterator1.next();
+        }
+        return null;
+    }
+   public ToDoItem getAllTasks (){
+        for (ToDoItem temp: toDoList1){
+            System.out.println(temp);
+
+        }
+        return null;
+    }
+    public ToDoItem getIncompleteTasks() {
+        for (ToDoItem temp2 : toDoList1) {
+            if (!temp2.isCompletionTask()) {
+                System.out.println(temp2);
+            }
+        }
+
+        return null;
+    }
+        public ToDoItem getCompleteTasks(){
+            for (ToDoItem temp3: toDoList1){
+                if (temp3.isCompletionTask()){
+                    System.out.println(temp3);
+                }
+            }
+            return null;
+        }
+        public void markTaskComplete () {
+            for (ToDoItem temp4 : toDoList1) {
+                if (!temp4.isCompletionTask()) {
+                    temp4.setCompletionTask(true);
+                }
+            }
+            return;
+        }
+        public void markTaskIncomplete(){
+            for (ToDoItem temp5: toDoList1){
+                if (temp5.isCompletionTask()){
+                    temp5.setCompletionTask(false);
+                }
+            }
+            return;
+            }
+    }
